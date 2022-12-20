239415c98d816d327e5476122e7c0fe5cea96835
Sunil-731820 <sunilkumargupta1170@gmail.com>
2022-12-20 17:25:15 +0530

    Coded:

---
+++ EmployManagementSystemUsingJSF/src/main/java/com/java/jsf/StudentManagementSystemUsingJSF/DatabaseOperation.java
@@ -0,0 +1,145 @@
+package com.java.jsf.StudentManagementSystemUsingJSF;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Map;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class DatabaseOperation {
+
+	public static Statement stmtObj;
+	public static Connection connObj;
+	public static ResultSet resultSetObj;
+	public static PreparedStatement pstmt;
+	
+//	EstabLishing The Connections in Database
+	public static Connection getConnection() throws SQLException {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");     
+            String db_url ="jdbc:mysql://localhost:3306/student_management_system_using_jsf",
+                    db_userName = "root",
+					db_password = "root";
+			connObj = DriverManager.getConnection(db_url,db_userName,db_password);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return connObj;
+	}
+	
+//	1 : Fetching The Data From the Database using Query
+	public static ArrayList getStudentsListFromDB() {
+		ArrayList studentList = new ArrayList();
+		try {
+			stmtObj = getConnection().createStatement();
+			resultSetObj = stmtObj.executeQuery("select * from student_record");
+			while(resultSetObj.next()) {
+				StudentBean stuObj = new StudentBean();
+				stuObj.setId(resultSetObj.getInt("student_id"));
+				stuObj.setName(resultSetObj.getString("student_name"));
+				stuObj.setEmail(resultSetObj.getString("student_email"));
+				stuObj.setPassword(resultSetObj.getString("student_password"));
+				stuObj.setGender(resultSetObj.getString("student_gender"));
+				stuObj.setAddress(resultSetObj.getString("student_address"));
+				studentList.add(stuObj);
+			}
+			
+			System.out.println("Total Records Fetched is "+ studentList.size());
+			connObj.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return studentList;
+	}
+	
+// 2 :	Saving the Student Records In Database 
+	public static String saveStudentDetailsInDB(StudentBean newStudentObj) {
+		int saveResult = 0;
+		String navigationResult = "";
+		try {
+			pstmt = getConnection().prepareStatement("insert into student_record (student_name, student_email, student_password, student_gender, student_address) values (?, ?, ?, ?, ?)");
+			pstmt.setString(1, newStudentObj.getName());
+			pstmt.setString(2, newStudentObj.getEmail());
+			pstmt.setString(3, newStudentObj.getPassword());
+			pstmt.setString(4, newStudentObj.getGender());
+			pstmt.setString(5,newStudentObj.getAddress());
+			saveResult = pstmt.executeUpdate();
+			connObj.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if(saveResult !=0) {
+			navigationResult = "studentsList.xhtml?faces-redirect=true";
+		}else {
+			navigationResult = "createStudent.xhtml?faces-redirect=true";
+		}
+		return navigationResult;
+	}
+	
+//	3 : updating the Data Using Id :
+	
+	public static String editStudentRecordInDB(int studentId) {
+        StudentBean editRecord = null;
+        System.out.println("editStudentRecordInDB() : Student Id: " + studentId);
+ 
+        /* Setting The Particular Student Details In Session */
+        Map<String,Object> sessionMapObj = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+ 
+        try {
+            stmtObj = getConnection().createStatement();    
+            resultSetObj = stmtObj.executeQuery("select * from student_record where student_id = "+studentId);    
+            if(resultSetObj != null) {
+                resultSetObj.next();
+                editRecord = new StudentBean(); 
+                editRecord.setId(resultSetObj.getInt("student_id"));
+                editRecord.setName(resultSetObj.getString("student_name"));
+                editRecord.setEmail(resultSetObj.getString("student_email"));
+                editRecord.setGender(resultSetObj.getString("student_gender"));
+                editRecord.setAddress(resultSetObj.getString("student_address"));
+                editRecord.setPassword(resultSetObj.getString("student_password")); 
+            }
+            sessionMapObj.put("editRecordObj", editRecord);
+            connObj.close();
+        } catch(Exception sqlException) {
+            sqlException.printStackTrace();
+        }
+        return "/editStudent.xhtml?faces-redirect=true";
+    }
+ 
+    public static String updateStudentDetailsInDB(StudentBean updateStudentObj) {
+        try {
+            pstmt = getConnection().prepareStatement("update student_record set student_name=?, student_email=?, student_password=?, student_gender=?, student_address=? where student_id=?");    
+            pstmt.setString(1,updateStudentObj.getName());  
+            pstmt.setString(2,updateStudentObj.getEmail());  
+            pstmt.setString(3,updateStudentObj.getPassword());  
+            pstmt.setString(4,updateStudentObj.getGender());  
+            pstmt.setString(5,updateStudentObj.getAddress());  
+            pstmt.setInt(6,updateStudentObj.getId());  
+            pstmt.executeUpdate();
+            connObj.close();            
+        } catch(Exception sqlException) {
+            sqlException.printStackTrace();
+        }
+        return "/studentsList.xhtml?faces-redirect=true";
+    }
+
+	public static String deleteStudentRecordInDB(int studentId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
